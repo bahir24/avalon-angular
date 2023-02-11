2a16000849077c617af6ae9a55b1d272f75920c6
bahir24 <m.bahirev@wemakefab.ru>
2023-02-11 08:01:34 +0300

    edit with backend

--- src/app/pages/auth/registration/registration.component.ts
+++ src/app/pages/auth/registration/registration.component.ts
@@ -5,8 +5,8 @@ import {IUser} from "../../../models/users";
 import {MessageService} from "primeng/api";
 import {LocalStorageService} from "../../../services/local-storage/local-storage.service";
 import {ConfigService} from "../../../services/config/config.service";
-import {HttpClient, HttpErrorResponse} from "@angular/common/http";
-import {ServerError} from "../../../models/errors";
+import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
+import {Message} from "primeng/api/message";
 
 @Component({
   selector: 'app-registration',
@@ -17,67 +17,125 @@ export class RegistrationComponent implements OnInit {
   public minLength = 6;
   public registrationForm: FormGroup;
   public isVariousPasswords = false;
-  public isLocalStorage = true;
   public useUserCard = false;
+  private usersUrl = "http://localhost:3000/users";
 
-  constructor(private authService: AuthService,
-              private messageService: MessageService,
-              private localStorageService: LocalStorageService,
-              private http: HttpClient) {
+  private message: Message = {
+    severity: 'success',
+    summary: 'Регистрация прошла успешно'
+  };
+
+  constructor(
+    private authService: AuthService,
+    private messageService: MessageService,
+    private localStorageService: LocalStorageService,
+    private http: HttpClient
+  ) {
     this.registrationForm = new FormGroup({
       "login": new FormControl('', Validators.required),
       "password": new FormControl('', [Validators.required, Validators.minLength(this.minLength)]),
-      "confirmPassword": new FormControl('', [Validators.required, Validators.minLength(this.minLength)]),
       "email": new FormControl('', [Validators.required, Validators.email]),
-      "isLocalStorage": new FormControl<boolean>(this.isLocalStorage),
       "cardNumber": new FormControl('',),
     });
   }
-
   ngOnInit() {
     this.useUserCard = ConfigService.config.useUserCard;
   }
 
-  registrationSubmit(user: IUser) {
-    if (this.registrationForm.controls['password'].value !== this.registrationForm.controls['confirmPassword'].value) {
-      this.isVariousPasswords = true;
-      return;
-    }
-    const userObj: IUser = {
+  registrationSubmit() {
+    const user = this.makeUser();
+
+    this.http.post<IUser>(this.usersUrl, user, {headers: new HttpHeaders('Accept: application/json')})
+      .subscribe((data: IUser) => window.localStorage.setItem('user_' + user.login, JSON.stringify(user)),
+        (msg: HttpErrorResponse) => this.message = {severity: 'warn', summary: msg.statusText, detail: 'msg.status'});
+
+    this.messageService.add(this.message);
+  }
+
+  makeUser(): IUser {
+    return {
       login: this.registrationForm.controls['login'].value,
       email: this.registrationForm.controls['email'].value,
       password: this.registrationForm.controls['password'].value,
       cardNumber: this.registrationForm.controls['cardNumber'].value,
     }
-
-
-    this.http.post<IUser>('http://localhost:4200/auth/users', userObj)
-      .subscribe((user: IUser) => {
-          this.isVariousPasswords = false;
-          this.authService.setUser(user);
-          if (this.isLocalStorage) {
-            this.setLocalStorage(user);
-          }
-          this.messageService.add({
-            severity: 'success',
-            summary: `${user.login} Регистрация ${!!this.registrationForm.controls['isLocalStorage'].value ? 'с сохранением в localStorage ' : ' '}прошла успешно.`,
-            life: 1500
-          });
-          this.registrationForm.reset();
-        },
-        (err: HttpErrorResponse) => {
-          const serverError: ServerError = err.error;
-          this.messageService.add({
-            severity: 'error',
-            summary: serverError.errorText,
-            life: 1500
-          });
-        })
   }
 
-  setLocalStorage(user: IUser) {
-    const userJson: string = JSON.stringify(user);
-    this.localStorageService.setLocalStorage(`user_${user.login}`, userJson);
-  }
 
 }
+
+
+// const objUserJsonStr = JSON.stringify(userObj);
+// window.localStorage.setItem('user_'+userObj.login, objUserJsonStr);
+
+
+// this.http.post(usersUrl, userObj, {}).subscribe(
+//   article => {
+//     console.log(article);
+//   }
+// );
+
+// const userObj: IUser = {
+//   login: this.registrationForm.controls['login'].value,
+//   email: this.registrationForm.controls['email'].value,
+//   password: this.registrationForm.controls['password'].value,
+//   cardNumber: this.registrationForm.controls['cardNumber'].value,
+// }
+// console.log('here');
+// if (this.registrationForm.controls['password'].value !== this.registrationForm.controls['confirmPassword'].value) {
+//   this.isVariousPasswords = true;
+//   return;
+// }
+
+//   reportProgress?: boolean;
+//   responseType?: 'arraybuffer' | 'blob' | 'json' | 'text';
+//   withCredentials?: boolean;
+// } = {}): Observable<any>
+
+// this.http.post(usersUrl, userObj, {'responseType': 'text'}).pipe(123)
+// .pipe((response: OperatorFunction<string, unknown>) => return true; );
+// tap( // Log the result or error
+//   {
+//     next: (data) => console.log(data),
+//     error: (error) => console.log(error)
+//   }
+// ):
+
+// this.http.post("http://localhost:3000/users", userObj).pipe(
+//
+// )
+
+// {};
+// reportProgress?: boolean; 'responseType'?: "json"; 'withCredentials'?: boolean; }): Observable<HttpResponse<Object>>
+
+
+// this.http.post<IUser>('http://localhost:3000/users', userObj)
+//   .subscribe({
+//     next: () => fail('should have failed with the 404 error'),
+//     error: (error: HttpErrorResponse) => {
+//       expect(error.status).withContext('status').toEqual(404);
+//       expect(error.error).withContext('message').toEqual('asdads');
+//     },
+//   })
+
+
+// this.isVariousPasswords = false;
+// this.authService.setUser(user);
+// if (this.isLocalStorage) {
+//   this.setLocalStorage(user);
+// }
+// this.messageService.add({
+//   severity: 'success',
+//   summary: `${user.login} Регистрация ${!!this.registrationForm.controls['isLocalStorage'].value ? 'с сохранением в localStorage ' : ' '}прошла успешно.`,
+//   life: 1500
+// });
+// this.registrationForm.reset();
+// },
+// (err: HttpErrorResponse) => {
+//   const serverError: ServerError = err.error;
+//   this.messageService.add({
+//     severity: 'error',
+//     summary: serverError.errorText,
+//     life: 1500
+//   });
+// })
